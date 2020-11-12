b0c62076344159a1c8159922e1fc594cf2e3b63e
Tommaso Massaza <tommymassa98@gmail.com>
2020-11-12 17:02:30 +0100

    Aggiungere i file di progetto.

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            int sum = Calculator.Sum(a, b);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+        }
+    }
+}
---
+++ CalculatorLib/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
